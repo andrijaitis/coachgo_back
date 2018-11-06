9cf5af081fb242e6a9f40bf90d6df565bbba93c5
andrijaitis <andrijaitis92@gmail.com>
2018-11-06 19:09:21 +0100

    backend heroku works

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (port === 3000) { // adds headers if on dev
+
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
+  res.setHeader('Access-Control-Allow-Origin', 'https://coachgo.herokuapp.com'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
-}
 
 var routes = require('./routes/router');
 
