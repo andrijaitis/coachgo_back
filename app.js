06047af479b6f3efc157ef6ad6219e8276c7a38d
andrijaitis <andrijaitis92@gmail.com>
2018-11-06 18:04:46 +0100

    make it better

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.use(session({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
 var routes = require('./routes/router');
 
@@ -52,6 +52,6 @@ app.use(function (err, req, res, next) {
   res.send(err.message);
 });
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Express app listening on port 3000');
 });
