32ccba48a1fe97208ac34a2af6336ced8e7490f6
andrijaitis <andrijaitis92@gmail.com>
2018-11-15 02:31:16 +0100

    origin

--- app.js
+++ app.js
@@ -15,28 +15,28 @@ db.once('open', function () {
 console.log('DB connect success');
 });
 
-const origin = 'https://coachgo.herokuapp.com';
-// var allowedOrigins = ['http://localhost:4200', 'https://coachgo.herokuapp.com'];
-if (port === 3000){origin = allowedOrigins[0]} else {origin = allowedOrigins[1]}
-app.use(cors({
-origin: [origin],
-credentials: true,
-}));
+// const origin = 'https://coachgo.herokuapp.com';
+// // var allowedOrigins = ['http://localhost:4200', 'https://coachgo.herokuapp.com'];
+// if (port === 3000){origin = allowedOrigins[0]} else {origin = allowedOrigins[1]}
+// app.use(cors({
+// origin: [origin],
+// credentials: true,
+// }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const origin = 'https://coachgo.herokuapp.com';
-// var allowedOrigins = ['http://localhost:4200', 'https://coachgo.herokuapp.com'];
-// // if (port === 3000){origin = allowedOrigins[0]} else {origin = allowedOrigins[1]}
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]); 
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
+const origin = 'https://coachgo.herokuapp.com';
+var allowedOrigins = ['http://localhost:4200', 'https://coachgo.herokuapp.com'];
+// if (port === 3000){origin = allowedOrigins[0]} else {origin = allowedOrigins[1]}
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', origin); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const routes = require('./routes/authentication');
 const athleteRoutes = require('./routes/athlete');
