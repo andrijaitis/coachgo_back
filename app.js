834d105d8405224153129fc242ecda669ddadbf7
andrijaitis <andrijaitis92@gmail.com>
2018-11-26 00:44:32 +0100

    n

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var allowedOrigins = ['http://localhost:4200', 'https://coachgo.herokuapp.com'];
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', allowedOrigins[0]); 
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigins[1]); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
